659559debab927558e5970ebab3f1c63fb4b006f
Andrey Souza <andreysouzastart@gmail.com>
2024-05-07 15:58:53 -0300

    Inclusão tela de login e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './navegacao/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './acessos/login/login.component';
+import { CadastroComponent } from './acessos/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'cadastro', component: CadastroComponent},
+  {
+    path: '',
+    children: [
+      { path:'', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent},
+    ]
+  }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
