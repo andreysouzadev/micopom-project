143b6efd07bea684493b61ec91a6686b28019e97
andrey <andreysouzastart@gmai.com>
2024-06-10 16:51:49 -0300

    Alterações de layout da home com filtros, e inclusão do search bar

--- src/app/my-account/manage-account/manage-account.component.ts
+++ src/app/my-account/manage-account/manage-account.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-my-coupons',
-  template: `
-    <div>
-      <app-my-account-menu></app-my-account-menu>
-      <div class="coupons-content">
-        <h2>Minha conta</h2>
-        <!-- Conteúdo dos meus cupons -->
-      </div>
-    </div>
-  `,
-  styles: [`
-    .coupons-content {
-      padding-left: 17%;
-      margin-top: 5%;
-    }
-  `]
+  selector: 'app-manage-accont',
+  templateUrl: './manage-account.component.html',
+  styleUrls: ['./manage-account.component.css']
 })
 
 export class ManageAccountComponent {
