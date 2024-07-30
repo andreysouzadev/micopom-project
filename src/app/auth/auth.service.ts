14f0fab18f67150d33c17e260d479b67a5ca3542
Diogo Vedovello <diogobalieirovedovello@gmail.com>
2024-07-30 20:21:23 -0300

    save

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 // auth.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, BehaviorSubject, Subject, of } from 'rxjs';
 import { environment } from 'src/environment';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs';
@@ -43,17 +44,21 @@ export class AuthService {
     this.tokenSubject.next(null);
   }
 
-  login(email: string, password: string){
-    return this.http.post<{token: string}>(`${this.apiUrl}/login`, { email, password }).subscribe(
-        response => {
+  login(email: string, password: string): Observable<any>{
+    return this.http.post<{token: string}>(`${this.apiUrl}/login`, { email, password }).pipe(
+        map(response => {
             if (response && response.token) {
                 this.setToken(response.token);
                 this.loadUserDetails();
-                this.router.navigate(['/home']);
+                return 200;
             } else {
+              return 204;
             }
-        }, error => {
-        }
+        }), 
+        catchError(err => {
+          console.log(err)
+          return of(204);
+        })
     );
   }
 
